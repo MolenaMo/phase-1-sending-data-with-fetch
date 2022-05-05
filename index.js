14db876894d2c3f63566faff473a87acb4e2a099
MLM <100675603+MolenaMo@users.noreply.github.com>
2022-05-05 02:29:47 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+const user = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+  
+  const userData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+  
+  function submitData() {
+    return fetch("http://localhost:3000/users", userData)
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((obj) => {
+        steveOnDOM(obj);
+      })
+      .catch((err) => {
+        alertMsg(err);
+      });
+  }
+  
+  function steveOnDOM(obj) {
+    console.log(obj);
+    let p = document.createElement("p");
+    p.innerHTML = `User: ${obj.name} | Email: ${obj.email} | Id: ${obj.id}`;
+    document.body.appendChild(p);
+  }
+  
+  function alertMsg(err) {
+    let h1 = document.createElement("h1");
+    h1.innerHTML = `${err}`;
+    document.body.appendChild(h1);
+  }
